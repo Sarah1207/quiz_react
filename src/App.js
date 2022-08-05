85929c533850579d6e30f9020375d9695220b10f
Sarah HAMI <sarah.hami07@gmail.com>
2022-08-05 15:49:00 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import questions from "./questions";
+import "./App.scss";
 
-function App() {
+export default function App() {
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [showScore, setShowScore] = useState(false);
+  const [score, setScore] = useState(0);
+
+  const handleClick = (isCorrect) => {
+    if (isCorrect === true) {
+      setScore(score + 1);
+    }
+    const next = currentQuestion + 1;
+    if (next < questions.length) {
+      setCurrentQuestion(next);
+    } else {
+      setShowScore(true);
+    }
+  };
+
+  const handleClickStartAgain = () => {
+    setCurrentQuestion(0);
+    setScore(0);
+    setShowScore(false);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app" data-testid="quiz">
+      {/* Score Screen */}
+      {showScore ? (
+        <div className="score-screen">
+          <div className="score-section">
+            Your score is {score} / {questions.length}
+          </div>
+          <div className="start-again">
+            {score < questions.length ? (
+              <button onClick={handleClickStartAgain}>Start again</button>
+            ) : (
+              <p> Congrats !</p>
+            )}
+          </div>
+        </div>
+      ) : (
+        // Question Screen
+        <>
+          <div className="question-section" data-testid="question-screen">
+            <div className="question-count">
+              <span>Question n°{currentQuestion + 1}</span>/{questions.length}
+            </div>
+            <div className="question-text">
+              {questions[currentQuestion].questionText}
+            </div>
+          </div>
+          <div className="answer-section">
+            {questions[currentQuestion].answerOptions.map(
+              (answerOption, key) => {
+                return (
+                  <button
+                    key={key}
+                    onClick={() => {
+                      handleClick(answerOption.isCorrect);
+                    }}
+                  >
+                    {answerOption.answerText}
+                  </button>
+                );
+              }
+            )}
+          </div>
+        </>
+      )}
     </div>
   );
 }
-
-export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,22 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("<App/>", () => {
+  test('should renders the word "Question"', () => {
+    render(<App />);
+    const element = screen.getByText(/Question/i);
+    expect(element).toBeInTheDocument();
+  });
+
+  test("should contain the data-testid 'quiz'", () => {
+    render(<App />);
+    const element = screen.getByTestId("quiz");
+    expect(element).toBeInTheDocument();
+  });
+
+  test("should contain the data-testid 'question-screen'", () => {
+    render(<App />);
+    const element = screen.getByTestId("question-screen");
+    expect(element).toBeInTheDocument();
+  });
 });
